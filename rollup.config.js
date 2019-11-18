9465ee29abc6701e7eca56e44dfc60a3a3dea560
Russell Johnson <russell.johnson@livio.io>
2019-11-18 11:37:14 -0500

    implement sdl packet. update rollup and package.json

--- rollup.config.js
+++ rollup.config.js
@@ -3,20 +3,20 @@ import babel from 'rollup-plugin-babel';
 import minify from 'rollup-plugin-babel-minify';
 
 export default {
-  input: 'lib/js/app.js',
-  output: [
-    {
-      file: 'lib/js/dist/SDL.js',
-      format: 'umd',
-      sourcemap: 'inline',
-      name: 'SDL'
-    },
-  ],
-  plugins: [
-    resolve(),
-    babel({
-      exclude: 'node_modules/**' // only transpile our source code
-    }),
-    minify({})
-  ]
+    input: 'lib/js/app.js',
+    output: [
+        {
+            file: 'lib/js/dist/SDL.js',
+            format: 'umd',
+            sourcemap: 'inline',
+            name: 'SDL',
+        },
+    ],
+    plugins: [
+        // resolve(),
+        babel({
+            exclude: 'node_modules/**', // only transpile our source code
+        }),
+        minify({}),
+    ],
 };
