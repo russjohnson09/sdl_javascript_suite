05ee5b5313ee87466d5521b16321616a358ec67e
Russell Johnson <russell.johnson@livio.io>
2019-11-18 10:34:31 -0500

    update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,56 +1,6 @@
-// import resolve from 'rollup-plugin-node-resolve';
-// import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
 import minify from 'rollup-plugin-babel-minify';
-
-// export default {
-//   input: 'lib/js/app.js',
-//   output: [
-//     {
-//       file: 'lib/js/dist/SDL.js',
-//       format: 'umd',
-//       sourcemap: 'inline',
-//       name: 'SDL'
-//     },
-//   ],
-//   plugins: [
-//     resolve(),
-//     babel({
-//       exclude: 'node_modules/**' // only transpile our source code
-//     }),
-//     minify({})
-//   ]
-// };
-
-
-
-//https://risanb.com/posts/bundling-your-javascript-library-with-rollup/
-
-//rollup -c
-//tested and working in browser.
-//common working in node, but missing node classes.
-// export default {
-//   input: 'lib/js/app.js',
-//   output: [
-//       {
-//           file: 'lib/js/dist/SDL.js',
-//           format: 'umd',
-//           name: 'sdl-js'
-//       }]
-// };
-
-//working with inline souremap
-// export default {
-//   input: 'lib/js/app.js',
-//   output: [
-//       {
-//       file: 'lib/js/dist/SDL.js',
-//       format: 'umd',
-//       sourcemap: 'inline',
-//       name: 'SDL'
-//       }]
-// };
-
-
 
 export default {
   input: 'lib/js/app.js',
@@ -62,10 +12,12 @@ export default {
       name: 'SDL'
       }],
     plugins: [
+      // https://github.com/rollup/rollup-plugin-node-resolve
+      // it sounds like some specific browser/node config should be set in the package for resolve to work.
       // resolve(),
-      // babel({
-        // exclude: 'node_modules/**' // only transpile our source code
-      // }),
+      babel({
+        exclude: 'node_modules/**' // only transpile our source code
+      }),
       minify({})
   ]
 };
