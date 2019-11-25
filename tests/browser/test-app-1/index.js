b71233b03e8e80067c348ad223de3ec4762284d8
unknown <russjohnson09@gmail.com>
2019-11-24 22:01:54 -0500

    exit

--- tests/browser/test-app-1/index.js
+++ tests/browser/test-app-1/index.js
@@ -96,10 +96,15 @@ app.ws('/', (ws, req) => {
     coreHost: process.env.CORE_HOST });
 });
 
+app.use(express.static(__dirname));
+
+app.use(express.static(__dirname + './../../../lib/js/dist/'));
 
 
 const PORT = process.env.PORT || 9090;
 let server = app.listen(PORT, async function() {
-    console.log(`app is listening`);
-
+    console.log(`app is listening`,PORT);
+    require("openurl").open("http://localhost:" + PORT);
 });
+
+
